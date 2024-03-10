d53b68166e92dc22037a919184901672870c55ea
Nahid Ahmed <npahmed92@gmail.com>
2024-03-10 04:55:53 -0400

    Add a prompt if input fields are empty when submitting

---
+++ Assets/js/logic.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES ======================================
+const formEl = document.querySelector('form');
+const blogForm = document.querySelector('.blog-form');
+const username = document.querySelector('#username');
+const title = document.querySelector('#title');
+const content = document.querySelector('#content-input');
+const submit = document.querySelector('#submit');
+
+// DATA
+
+// FUNCTIONS
+function formValidation(event) {
+  event.preventDefault();
+  if (!username.value || !title.value || !content.value) {
+    alert('All fields are required to post.');
+  }
+}
+
+// USER INTERACTIONS
+submit.addEventListener('click', formValidation);
