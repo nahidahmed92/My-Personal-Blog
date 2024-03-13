5d769ba7cb717fcd01925c4439b8958f0e8fdfab
Nahid Ahmed <npahmed92@gmail.com>
2024-03-12 23:44:34 -0400

    push before deadline

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,4 +1,22 @@
+// DEPENDENCIES (DOM Elements) =======================
+const section = document.querySelectorAll('section');
+
+// DATA ==============================================
+const blogPosts = JSON.parse(localStorage.getItem('blogPosts'));
+
+// FUNCTIONS =========================================
 function goBack() {
   // You can specify the source URL you want to go back to
   window.history.go(-1); // This will go back one page in the history
 }
+
+// USER INTERACTIONS =================================
+// INITIALIZATION ====================================
+
+// for ()
+
+// create
+
+// build
+
+// place
--- assets/js/form.js
+++ assets/js/form.js
@@ -2,6 +2,7 @@
 const usernameInput = document.querySelector('#username');
 const titleInput = document.querySelector('#title');
 const contentInput = document.querySelector('#content');
+const blogForm = document.querySelector('.blog-form');
 const submitBtn = document.querySelector('#submit');
 
 // DATA ==============================================
@@ -15,24 +16,25 @@ console.log(blogPosts);
 // USER INTERACTIONS =================================
 submitBtn.addEventListener('click', function (event) {
   event.preventDefault();
+  console.log('username input: ', usernameInput.value);
+  if (!usernameInput.value || !titleInput.value || !contentInput.value) {
+    console.log('fields are empty');
+    alert('Please ensure all fields are filled before submitting a post.');
+  } else {
+    // DATA ============================================
+    const newBlogPost = {
+      username: usernameInput.value,
+      title: titleInput.value,
+      content: contentInput.value,
+    };
 
-  // if (usernameInput.value === null) {
-  //   alert('all fields are required');
-  // }
-
-  // DATA ============================================
-  const newBlogPost = {
-    username: usernameInput.value,
-    title: titleInput.value,
-    content: contentInput.value,
-  };
-
-  blogPosts.push(newBlogPost);
-  console.log(newBlogPost);
-  localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
-  // localStorage.setItem('blogForm', JSON.stringify(blogForm));
-  // blogPosts.push(blogForm);
-  window.location.href = 'blog.html';
+    blogPosts.push(newBlogPost);
+    console.log(newBlogPost);
+    localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
+    // localStorage.setItem('blogForm', JSON.stringify(blogForm));
+    // blogPosts.push(blogForm);
+    window.location.href = 'blog.html';
+  }
 });
 
 // INITIALIZATION ====================================
